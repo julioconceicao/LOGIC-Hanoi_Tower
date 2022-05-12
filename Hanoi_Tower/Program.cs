dc8bd72b54b95aa7e6db10bf3b578e62638877a4
Julio César G. Conceição <julioconceicao.gtr@gmail.com>
2022-05-12 17:50:56 -0300

    [First Uploading] {May.12.2022}

---
+++ Hanoi_Tower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hanoi_Tower {
+    class Program {
+        static void Main(string[] args) {
+
+            char startPeg = 'A'; // start tower in output
+            char endPeg = 'C'; // end tower in output
+            char tempPeg = 'B'; // temporary tower in output
+            int totalDisks = 5; // number of disks
+
+            solveTowers(totalDisks, startPeg, endPeg, tempPeg);
+        }
+
+        private static void solveTowers(
+            int n,
+            char startPeg,
+            char endPeg,
+            char tempPeg
+        ) {
+            if (n > 0) {
+                solveTowers(n - 1, startPeg, tempPeg, endPeg);
+                Console.WriteLine("Move disk from " + startPeg + ' ' + endPeg);
+                solveTowers(n - 1, tempPeg, endPeg, startPeg);
+            }
+        }
+    }
+}
